640fdeadcfb0e9b2615ae961e2c002102d4d665d
Matt Stedman <67828580+Matt-Stedman@users.noreply.github.com>
2023-01-21 20:53:13 +0100

    Update ready for Ilyes

--- main.py
+++ main.py
@@ -1,107 +1,121 @@
-bluetooth.onBluetoothConnected(function () {
-    basic.showString("O")
-    basic.showLeds(`
-        # . . . .
-        # . . . .
-        # # # . .
-        # . . . .
-        # . . . .
-        `)
-    serial.writeLine("Connected to phone via bluetooth")
-    is_bt_connected = true
-})
-bluetooth.onBluetoothDisconnected(function () {
-    basic.showLeds(`
-        . . . . #
-        . . . . #
-        . . # # #
-        . . . . #
-        . . . . #
-        `)
-    serial.writeLine("Disconnected Bluetooth")
-    is_bt_connected = false
-})
-input.onSound(DetectedSound.Loud, function () {
-    if (sound_mode == true && is_bt_connected) {
+def on_gesture_eight_g():
+    if sound_mode == False and tolerance == "8g":
         hit_recieved()
-    } else {
-        serial.writeLine("Not BT connected/no sound")
-    }
-})
-bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
-    command = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
-    serial.writeLine(command)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
+input.on_gesture(Gesture.EIGHT_G, on_gesture_eight_g)
+
+def on_bluetooth_connected():
+    global is_bt_connected
+    basic.show_string("O")
+    basic.show_leds("""
         # . . . .
+                # . . . .
+                # # # . .
+                # . . . .
+                # . . . .
+    """)
+    serial.write_line("Connected to phone via bluetooth")
+    is_bt_connected = True
+bluetooth.on_bluetooth_connected(on_bluetooth_connected)
+
+def on_bluetooth_disconnected():
+    global is_bt_connected
+    basic.show_leds("""
+        . . . . #
+                . . . . #
+                . . # # #
+                . . . . #
+                . . . . #
+    """)
+    serial.write_line("Disconnected Bluetooth")
+    is_bt_connected = False
+bluetooth.on_bluetooth_disconnected(on_bluetooth_disconnected)
+
+def on_gesture_six_g():
+    if sound_mode == False and tolerance == "6g":
+        hit_recieved()
+input.on_gesture(Gesture.SIX_G, on_gesture_six_g)
+
+def on_sound_loud():
+    if sound_mode == True:
+        hit_recieved()
+input.on_sound(DetectedSound.LOUD, on_sound_loud)
+
+def on_uart_data_received():
+    global command, tolerance, rate, sound_mode
+    command = bluetooth.uart_read_until(serial.delimiters(Delimiters.HASH))
+    serial.write_line("" + (command))
+    basic.show_leds("""
         . . . . .
-        `)
+                . . . . .
+                . . . . .
+                # . . . .
+                . . . . .
+    """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . . . . .
-        . . . . .
-        . . . . .
-        # . # . .
-        . . . . .
-        `)
+                . . . . .
+                . . . . .
+                # . # . .
+                . . . . .
+    """)
     basic.pause(100)
-    basic.showLeds(`
+    basic.show_leds("""
         . . . . .
-        . . . . .
-        . . . . .
-        # . # . #
-        . . . . .
-        `)
+                . . . . .
+                . . . . .
+                # . # . #
+                . . . . .
+    """)
     basic.pause(100)
-    if (command.includes("TOLE")) {
-        tolerance = parseFloat(command.split(":")[1])
-        basic.showString("T")
-    } else if (command.includes("RATE")) {
-        rate = parseFloat(command.split(":")[1])
-        basic.showString("R")
-    } else if (command.includes("SNDM")) {
-        sound_mode = 1 == parseFloat(command.split(":")[1])
-        basic.showString("S")
-    }
+    if command.includes("TOLE"):
+        tolerance = command.split(":")[1]
+        basic.show_string("T=" + tolerance)
+    elif command.includes("RATE"):
+        rate = parse_float(command.split(":")[1])
+        basic.show_string("R=" + str(rate))
+    elif command.includes("SNDM"):
+        sound_mode = 1 == parse_float(command.split(":")[1])
+        basic.show_string("S=" + str(sound_mode))
     basic.pause(500)
-    basic.clearScreen()
-})
-function hit_recieved () {
-    if (input.runningTime() > last_punch_time + rate) {
-        bluetooth.uartWriteString("HIT")
-        last_punch_time = input.runningTime()
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        serial.writeLine("HIT")
-    }
-    basic.pause(3)
-    basic.clearScreen()
-}
-input.onGesture(Gesture.ThreeG, function () {
-    if (sound_mode == false && is_bt_connected) {
+    basic.clear_screen()
+bluetooth.on_uart_data_received(serial.delimiters(Delimiters.HASH), on_uart_data_received)
+
+def hit_recieved():
+    global last_punch_time
+    if is_bt_connected:
+        if input.running_time() > last_punch_time + rate:
+            bluetooth.uart_write_string("HIT")
+            last_punch_time = input.running_time()
+            basic.show_leds("""
+                # . . . #
+                                . # . # .
+                                . . # . .
+                                . # . # .
+                                # . . . #
+            """)
+            serial.write_line("HIT")
+        basic.pause(3)
+        basic.clear_screen()
+    else:
+        serial.write_line("Not BT connected/on sound mode")
+
+def on_gesture_three_g():
+    if sound_mode == False and tolerance == "3g":
         hit_recieved()
-    } else {
-        serial.writeLine("Not BT connected/on sound mode")
-    }
-})
-let last_punch_time = 0
-let command = ""
-let is_bt_connected = false
-let sound_mode = false
-let rate = 0
-let tolerance = ""
-basic.showString("Sens.ai")
-bluetooth.startUartService()
+input.on_gesture(Gesture.THREE_G, on_gesture_three_g)
+
+last_punch_time = 0
+command = ""
+is_bt_connected = False
+sound_mode = False
+rate = 0
+tolerance = ""
+basic.show_string("Sens.ai")
+bluetooth.start_uart_service()
 tolerance = "3g"
-let average_g = 1024
+average_g = 1024
 rate = 20
-sound_mode = false
-let all_in_1000ms: number[] = []
-let game_mode = "gameHRP"
+sound_mode = False
+all_in_1000ms: List[number] = []
+game_mode = "gameHRP"
